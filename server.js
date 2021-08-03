45902f2f02005a8caae2263f0be3b551d55b111e
Karim Balayev <karimbalayev@balayevkerim.local>
2021-08-03 10:57:15 +0200

    asaf

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ mongoose
     console.log('Connected to Databse');
   });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, (err) => {
   console.log('Server started');
   if (err) {
